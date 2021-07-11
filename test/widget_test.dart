35bd19b3185fb6d0d9fe696e4a0d98f7cf1ee113
Alexandr <digital-mag@ya.ru>
2021-07-11 10:36:00 +0300

    fix tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,11 +9,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:some_app/main_page.dart';
+import 'package:some_app/models/dao/tasks_dao.dart';
+import 'package:some_app/models/database/database.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+
+//    WidgetsFlutterBinding.ensureInitialized();
+    final database = await $FloorAppDatabase.databaseBuilder('app_database.db').build();
+    final TaskDao tasks = database.taskDao;
+
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const App());
+    await tester.pumpWidget(App(tasks));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
